0f5b8a03d9f69b325515fcffc18c686c24d8444a
Brian <brian.k.isaac-medina@durham.ac.uk>
2021-09-01 14:26:55 +0100

    C4C5

--- configs/bmvc2021_rebuttal/yolov3_mvvitdarknet53_608_30e_db4_AC_d1c4c5_shared.py
+++ configs/bmvc2021_rebuttal/yolov3_mvvitdarknet53_608_30e_db4_AC_d1c4c5_shared.py
@@ -1,7 +1,7 @@
 _base_ = 'yolov3_mvvitdarknet53_608_30e_db4_AC_sv.py'
 model = dict(
     backbone=dict(
-        combination_block=[3, 4, 5],
+        combination_block=[4, 5],
         num_decoder_layers=1,
         shared_transformer=True
     )
