4db07d3434219638672bc921a16f6147783b04d4
Brian Kostadinov Shalon Isaac Medina <pfvn47@login1.bede.dur.ac.uk>
2022-01-24 12:04:07 +0000

    Missing configs

---
+++ configs/icpr2022/yolov3_mvvitdarknet53_608_30e_wildtrack_d1c4_cat_shared_nopretrain.py
@@ -0,0 +1,9 @@
+_base_ = 'yolov3_mvvitdarknet53_608_30e_wildtrack_sv_nopretrain.py'
+model = dict(
+    backbone=dict(
+        combination_block=4,
+        num_decoder_layers=1,
+        multiview_decoder_mode='cat',
+        shared_transformer=True
+    )
+)
